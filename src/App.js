12ce705a5f6d21898320d8c3ca6f018720c5e341
antondor <user@MacBook-Pro-User.local>
2023-02-12 00:41:03 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Block } from './Block.jsx'
 
 function App() {
+
+  const [currency1, setCurrency1] = React.useState('UAH');
+  const [currency2, setCurrency2] = React.useState('USD');
+  const [price1, setPrice1] = React.useState(0);
+  const [price2, setPrice2] = React.useState(0);
+
+  const [data, setData] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://api.currencyapi.com/v3/latest?apikey=2XTrA18xlYylcLgfsMBp6OgA9th6JLfZBUgWpEnp')
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json.data);
+        console.log(json.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Unable to fetch the data');
+      });
+  } , []);
+
+  const onChangePrice1 = (value1) => {
+    const price = value1 / data[currency1].value;
+    const result = price * data[currency2].value;
+    setPrice2(result);
+    setPrice1(value1);
+  }
+
+  const onChangePrice2 = (value) => {
+    const result = (data[currency1].value / data[currency2].value) * value;
+    setPrice1(result)
+    setPrice2(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block value={price1} currency={currency1} onChangeCurrency={setCurrency1} onChangeValue={onChangePrice1} />
+      <Block value={price2} currency={currency2} onChangeCurrency={setCurrency2} onChangeValue={onChangePrice2} />
     </div>
   );
 }
